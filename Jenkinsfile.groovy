ec6e81f5c3dcd8a7572aa0e8fac01aa4cb10af23
Aadav <34786994+aadavvv@users.noreply.github.com>
2024-03-08 12:54:57 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,4 +11,14 @@ Jenkins.instance.getAllItems().each { job ->
     }
 }
 
+Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder.class).each { folder ->
+    folder.getItems().each { job ->
+        job.getBuilds().each { build ->
+            if (build.getTime().after(startOfDay)) {
+                totalJobsTriggered++
+            }
+        }
+    }
+}
+
 println "Total number of jobs triggered today: ${totalJobsTriggered}"
