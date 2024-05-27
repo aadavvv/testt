d049d6f55ad9bf45d15b57841486c237d8f2d1ee
Aadav <34786994+aadavvv@users.noreply.github.com>
2024-05-27 12:25:51 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,32 +1,14 @@
-@Grab(group='org.seleniumhq.selenium', module='selenium-java', version='3.141.59')
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.By
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.WebElement
+// This script will list all active inline pipeline jobs in Jenkins
 
-// Set the path to chromedriver executable
-System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe")
+// Function to check if a job is an inline pipeline job
+boolean isInlinePipelineJob(job) {
+    return job instanceof org.jenkinsci.plugins.workflow.job.WorkflowJob && job.definition instanceof org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
+}
 
-// Initialize ChromeDriver
-WebDriver driver = new ChromeDriver()
-
-// Open Jenkins URL
-driver.get("http://your-jenkins-url")
-
-// Find the username and password fields and login button
-WebElement usernameField = driver.findElement(By.xpath("//input[@id='j_username']"))
-WebElement passwordField = driver.findElement(By.xpath("//input[@name='j_password']"))
-WebElement loginButton = driver.findElement(By.xpath("//input[@name='Submit']"))
-
-// Enter username and password
-usernameField.sendKeys("your-username")
-passwordField.sendKeys("your-password")
-
-// Click on the login button
-loginButton.click()
-
-// Wait for some time to let the login process complete
-Thread.sleep(5000)
-
-// Close the browser
-driver.quit()
+// Iterate over all jobs and print the names of active inline pipeline jobs
+def jenkins = Jenkins.instance
+jenkins.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).each { job ->
+    if (job.isBuildable() && isInlinePipelineJob(job)) {
+        println "Active Inline Pipeline Job: ${job.fullName}"
+    }
+}
