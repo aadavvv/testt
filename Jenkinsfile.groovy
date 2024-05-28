4c405c1202b7e0a398e9c12c2a4827cab5661e19
Aadav <34786994+aadavvv@users.noreply.github.com>
2024-05-28 14:36:57 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,13 @@
-// This script will list all active inline pipeline jobs in Jenkins
+// Get the Jenkins instance
+def jenkinsInstance = Jenkins.getInstance()
 
-// Function to check if a job is an inline pipeline job
-boolean isInlinePipelineJob(job) {
-    return job instanceof org.jenkinsci.plugins.workflow.job.WorkflowJob && job.definition instanceof org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
-}
+// Get the Plugin Manager
+def pluginManager = jenkinsInstance.getPluginManager()
 
-// Iterate over all jobs and print the names of active inline pipeline jobs
-def jenkins = Jenkins.instance
-jenkins.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).each { job ->
-    if (job.isBuildable() && isInlinePipelineJob(job)) {
-        println "Active Inline Pipeline Job: ${job.fullName}"
-    }
+// Get the list of all installed plugins
+def plugins = pluginManager.getPlugins()
+
+// Iterate through the list and print plugin name and version
+plugins.each { plugin ->
+    println("${plugin.getShortName()}: ${plugin.getVersion()}")
 }
